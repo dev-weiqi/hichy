f7a6081dc26ef7b3726c992a032ea5c7a89c24a4
WEIQI WANG <dev.weiqi@gmail.com>
2021-06-05 16:36:23 +0800

    Fix sample.

--- app/src/main/java/dev/weiqi/hichysample/activity/MainActivity.kt
+++ app/src/main/java/dev/weiqi/hichysample/activity/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import dev.weiqi.hichy.Hichy
 import dev.weiqi.hichysample.R
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +15,6 @@ class MainActivity : AppCompatActivity() {
             setOnClickListener {
                 val intent = Intent(this@MainActivity, SecondActivity::class.java)
                 startActivity(intent)
-                Hichy.init()
             }
         }
     }
